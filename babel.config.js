d2ffe97555eb4963474bf439c9367758d990158d
Nancy <52145579+aLiz-Nancy@users.noreply.github.com>
2022-08-01 12:40:37 +0900

    feat(setup): initial project

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,26 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '~': './',
+            '~fonts': './src/assets/fonts/',
+            '~images': './src/assets/images/',
+            '~components': './src/components/',
+            '~constants': './src/constants/',
+            '~data/*': './src/data/',
+            '~hooks': './src/hooks/',
+            '~models': './src/models/',
+            '~navigation': './src/navigation/',
+            '~screens': './src/screens/',
+          },
+        },
+      ],
+    ],
   };
 };
